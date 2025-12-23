3c18aab47d5b93e8cfd2e694b45951f66d53e5cb
Gerald Collaku <collaku.gerald@gmail.com>
2025-12-23 14:47:15 +0100

    `RemoteFeedLoader` does not request data from URL

---
+++ HackerNewsFeed/HackerNewsFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  HackerNewsFeed
+//
+//  Created by Gerald Collaku on 23.12.25.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+                
+        XCTAssertNil(client.requestedURL)
+    }
+}
