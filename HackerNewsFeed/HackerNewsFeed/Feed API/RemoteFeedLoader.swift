cb3070e1933f5bdfd741ed95a7ebecbe216ebfb6
Gerald Collaku <collaku.gerald@gmail.com>
2025-12-25 18:52:16 +0100

    Replace magic number with variable giving extra context

--- HackerNewsFeed/HackerNewsFeed/Feed API/RemoteFeedLoader.swift
+++ HackerNewsFeed/HackerNewsFeed/Feed API/RemoteFeedLoader.swift
@@ -45,9 +45,11 @@ public final class RemoteFeedLoader {
 
 private class FeedItemsMapper {
     typealias Item = Int
+    
+    static var OK_200: Int { 200 }
 
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         let items = try JSONDecoder().decode([Item].self, from: data)
