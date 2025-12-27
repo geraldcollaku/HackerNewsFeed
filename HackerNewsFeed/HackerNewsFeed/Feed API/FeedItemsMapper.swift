43606766014478dd4af8146581fbdf468aedd22c
Gerald Collaku <collaku.gerald@gmail.com>
2025-12-27 10:59:44 +0100

    Replace `RemoteFeedLoader.Result` with the predefined `LoadFeedResult` enu from the Feed module (in preparation to conform to FeedLoader protocol)

--- HackerNewsFeed/HackerNewsFeed/Feed API/FeedItemsMapper.swift
+++ HackerNewsFeed/HackerNewsFeed/Feed API/FeedItemsMapper.swift
@@ -14,7 +14,7 @@ enum FeedItemsMapper {
     
     static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
         guard response.statusCode == OK_200, let root = try? JSONDecoder().decode([Item].self, from: data) else {
-            return .failure(.invalidData)
+            return .failure(RemoteFeedLoader.Error.invalidData)
         }
         
         let items = root.map { FeedItem(id: $0) }
--- HackerNewsFeed/HackerNewsFeed/Feed Feature/FeedLoader.swift
+++ HackerNewsFeed/HackerNewsFeed/Feed Feature/FeedLoader.swift
@@ -5,11 +5,15 @@
 //  Created by Gerald Collaku on 23.12.25.
 //
 
-enum LoadFeedResult {
-    case success([String])
-    case error(Error)
+public enum LoadFeedResult<Error: Swift.Error> {
+    case success([FeedItem])
+    case failure(Error)
 }
 
+extension LoadFeedResult: Equatable where Error: Equatable {}
+
 protocol FeedLoader {
-    func load(completion: @escaping (LoadFeedResult) -> Void)
+    associatedtype Error: Swift.Error
+    
+    func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
 }
