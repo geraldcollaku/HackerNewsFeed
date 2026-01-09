ff4e83a26ea262d9e86cdac1e5298b52ad4fed98
Gerald Collaku <collaku.gerald@gmail.com>
2026-01-09 19:25:35 +0100

    Save command requests cache deletion

--- HackerNewsFeed/HackerNewsFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ HackerNewsFeed/HackerNewsFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,23 +6,48 @@
 //
 
 import XCTest
+import HackerNewsFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
-    var deleteCacheCallCount = 0
+    var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
-
 
 class CacheFeedUseCaseTests: XCTestCase {
     
     func test_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
-        XCTAssertEqual(store.deleteCacheCallCount, 0)
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(0), uniqueItem(1)]
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func uniqueItem(_ id: Int) -> FeedItem {
+        FeedItem(id: id)
     }
 }
