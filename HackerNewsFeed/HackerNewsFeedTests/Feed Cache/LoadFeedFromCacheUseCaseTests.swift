f0bd5dd9bbba4f4a2ec220fc15a9255c86227c3b
Gerald Collaku <collaku.gerald@gmail.com>
2026-01-16 17:19:49 +0100

    Load command delivers no ids on more than seven days old cache

--- HackerNewsFeed/HackerNewsFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ HackerNewsFeed/HackerNewsFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -63,6 +63,17 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    func test_load_deliversNoIdsOnMoreThanSevenDaysOldCache() {
+        let feed = uniqueIdFeed()
+        let fixedCurrentDate = Date()
+        let moreThanSevenDaysTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: feed.local, timestamp: moreThanSevenDaysTimestamp)
+        })
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
