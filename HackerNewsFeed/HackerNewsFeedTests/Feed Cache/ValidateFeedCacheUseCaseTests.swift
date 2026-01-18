18add6896030b15cf043ba8db3ce7129d8e8d006
Gerald Collaku <collaku.gerald@gmail.com>
2026-01-18 20:19:57 +0100

    Validate cache command does not delete less than seven days old cache

--- HackerNewsFeed/HackerNewsFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ HackerNewsFeed/HackerNewsFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -34,6 +34,18 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_load_doesNotDeleteLessThanSevenDaysOldCache() {
+        let feed = uniqueIdFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -48,4 +60,24 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         NSError(domain: "any", code: 0)
     }
     
+    private func uniqueIdFeed() -> (models: [FeedId], local: [LocalFeedId]) {
+        let models = [uniqueId(0), uniqueId(1)]
+        let local = models.map { LocalFeedId(id: $0.id)}
+        return (models, local)
+    }
+    
+    private func uniqueId(_ id: Int) -> FeedId {
+        FeedId(id: id)
+    }
 }
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        self + seconds
+    }
+}
+
