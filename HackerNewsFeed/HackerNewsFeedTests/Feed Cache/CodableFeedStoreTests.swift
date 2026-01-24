88a1429c0771b26d1ae604364c40ea5085347646
Gerald Collaku <collaku.gerald@gmail.com>
2026-01-24 20:25:22 +0100

    Retrieving from empty cache delivers empty results

---
+++ HackerNewsFeed/HackerNewsFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,35 @@
+//
+//  CodableFeedStoreTests.swift
+//  HackerNewsFeed
+//
+//  Created by Gerald Collaku on 24.01.26.
+//
+
+import XCTest
+import HackerNewsFeed
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
+
+class CodableFeedStoreTests: XCTestCase {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "wait for cache retrieval")
+        
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+            default:
+                XCTFail("Expected empty result got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+}
