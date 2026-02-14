0813311fd2e0ff7dacd9b7b9bd5156d8f567097f
Gerald Collaku <collaku.gerald@gmail.com>
2026-02-14 13:35:20 +0100

    Clean up and undo store side-effects on `setUp` and `tearDown` to avoid sharing state betwen tests

--- HackerNewsFeed/HackerNewsFeedCacheIntegrationTests/HackerNewsFeedCacheIntegrationTests.swift
+++ HackerNewsFeed/HackerNewsFeedCacheIntegrationTests/HackerNewsFeedCacheIntegrationTests.swift
@@ -10,6 +10,18 @@ import HackerNewsFeed
 
 final class HackerNewsFeedCacheIntegrationTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
+    
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
@@ -40,6 +52,18 @@ final class HackerNewsFeedCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     private func testSpecificStoreURL() -> URL {
