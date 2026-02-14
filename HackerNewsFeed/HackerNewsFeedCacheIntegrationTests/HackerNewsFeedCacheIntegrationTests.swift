effc52046e6a430988e935a758b34ef20b1f152b
Gerald Collaku <collaku.gerald@gmail.com>
2026-02-14 13:36:08 +0100

    `LocalFeedLoader` in integration with `CoreDataFeedStore` delivers items saved on separate instances

--- HackerNewsFeed/HackerNewsFeedCacheIntegrationTests/HackerNewsFeedCacheIntegrationTests.swift
+++ HackerNewsFeed/HackerNewsFeedCacheIntegrationTests/HackerNewsFeedCacheIntegrationTests.swift
@@ -42,6 +42,37 @@ final class HackerNewsFeedCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueIdFeed().models
+        
+        let saveExp = expectation(description: "Wait for save completion")
+        sutToPerformSave.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(loadedFeed):
+                XCTAssertEqual(loadedFeed, feed)
+                
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+                
+            @unknown default:
+                XCTFail("Received unexpected case")
+            }
+            
+            loadExp.fulfill()
+        }
+        wait(for: [loadExp], timeout: 1.0)
+    }
+
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> LocalFeedLoader {
