a1d8abd487362014db1f76c071341aa0789e7402
Gerald Collaku <collaku.gerald@gmail.com>
2026-02-14 13:07:30 +0100

    Add `HackerNewsFeedCacheIntegrationTests` target to separate the potential slower cache integration tests from the fast unit/isolated tests.

---
+++ HackerNewsFeed/HackerNewsFeedCacheIntegrationTests/HackerNewsFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  HackerNewsFeedCacheIntegrationTests.swift
+//  HackerNewsFeedCacheIntegrationTests
+//
+//  Created by Gerald Collaku on 12.02.26.
+//
+
+import XCTest
+
+final class HackerNewsFeedCacheIntegrationTests: XCTestCase {
+    
+
+}
