03ecf8bc20d3c962d05920ed2e82be001cd96ead
Gerald Collaku <collaku.gerald@gmail.com>
2026-03-06 15:59:11 +0100

    Update timeout

--- HackerNewsFeed/HackerNewsFeedAPIEndToEndTests/HackerNewsFeedAPIEndToEndTests.swift
+++ HackerNewsFeed/HackerNewsFeedAPIEndToEndTests/HackerNewsFeedAPIEndToEndTests.swift
@@ -46,7 +46,7 @@ final class HackerNewsFeedAPIEndToEndTests: XCTestCase {
             receivedResult = result
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 10.0)
         
         return receivedResult
     }
