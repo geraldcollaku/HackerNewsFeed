7c9b729333017bc3dc1da616fa689d0998e4bdb4
Gerald Collaku <collaku.gerald@gmail.com>
2026-03-07 16:01:24 +0100

    Extract adapter pattern into a separate function to clarify intent

--- HackerNewsFeed/HackerNewsFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ HackerNewsFeed/HackerNewsFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,12 +12,15 @@ public enum FeedUIComposer {
     public static func feedComposedWith(loader: FeedLoader, storyLoader: StoryLoader) -> FeedViewController {
         let refreshController = FeedRefreshViewController(feedIdLoader: loader)
         let feedController = FeedViewController(refreshController: refreshController)
-        refreshController.onRefresh = { [weak feedController] feed in
-            feedController?.tableModel = feed.map { model in
-                FeedStoryCellController(model: model, storyLoader: storyLoader)
+        refreshController.onRefresh = adaptFeedToCellControllers(forwardingTo: feedController, loader: storyLoader)
+        return feedController
+    }
+    
+    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: StoryLoader) -> ([FeedId]) -> Void {
+        return { [weak controller] feed in
+            controller?.tableModel = feed.map { model in
+                FeedStoryCellController(model: model, storyLoader: loader)
             }
         }
-        
-        return feedController
     }
 }
