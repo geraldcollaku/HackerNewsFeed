2d51f84825b109b7dbce1f96b9ac8bf504e2b4c5
Gerald Collaku <collaku.gerald@gmail.com>
2026-03-15 12:13:11 +0100

    Move tableView datasource setup to storyboard

--- HackerNewsFeed/HackerNewsFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ HackerNewsFeed/HackerNewsFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -24,9 +24,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.prefetchDataSource = self
-        
+                
         onViewDidAppear = { vc in
             vc.onViewDidAppear = nil
             vc.refresh()
