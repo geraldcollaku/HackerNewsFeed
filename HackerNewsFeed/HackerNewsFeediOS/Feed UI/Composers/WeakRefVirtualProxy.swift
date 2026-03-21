cd9868dcc08c8e85948a2a147b0da914459165af
Gerald Collaku <collaku.gerald@gmail.com>
2026-03-21 17:50:25 +0100

    Move WeakRefVirtualProxy to its own file

---
+++ HackerNewsFeed/HackerNewsFeediOS/Feed UI/Composers/WeakRefVirtualProxy.swift
@@ -0,0 +1,28 @@
+//
+//  WeakRefVirtualProxy.swift
+//  HackerNewsFeed
+//
+//  Created by Gerald Collaku on 21.03.26.
+//
+
+import HackerNewsFeed
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedStoryView where T: FeedStoryView {
+    func display(_ viewModel: FeedStoryViewModel) {
+        object?.display(viewModel)
+    }
+}
