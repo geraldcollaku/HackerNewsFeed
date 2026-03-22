18e3139bbb07ad3d71395d694bad1939be7c2843
Gerald Collaku <collaku.gerald@gmail.com>
2026-03-22 12:40:32 +0100

    Move `ErrorView ` to a separate file

---
+++ HackerNewsFeed/HackerNewsFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  HackerNewsFeed
+//
+//  Created by Gerald Collaku on 22.03.26.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
