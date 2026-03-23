995dd7f98459276300d85373269d018a792fa90d
Gerald Collaku <collaku.gerald@gmail.com>
2026-03-23 09:34:45 +0100

    FeedStoryPresenter displays no story and loading view on didStartLoadingStory

--- HackerNewsFeed/HackerNewsFeedTests/Feed Presentation/FeedStoryPresenterTests.swift
+++ HackerNewsFeed/HackerNewsFeedTests/Feed Presentation/FeedStoryPresenterTests.swift
@@ -8,35 +8,45 @@
 import XCTest
 import HackerNewsFeed
 
-struct FeedStoryViewModel {
+struct FeedStoryViewModel: Hashable {
     let author: String?
     let title: String?
     let score: String?
     let url: String?
     
-    let isLoading: Bool
-    let shouldRetry: Bool
+    static var noStory: FeedStoryViewModel {
+        FeedStoryViewModel(
+            author: nil,
+            title: nil,
+            score: nil,
+            url: nil)
+    }
 }
 
 protocol FeedStoryView {
     func display(_ viewModel: FeedStoryViewModel)
 }
 
+struct FeedStoryLoadingViewModel {
+    let isLoading: Bool
+}
+
+protocol FeedStoryLoadingView {
+    func display(_ viewModel: FeedStoryLoadingViewModel)
+}
+
 final class FeedStoryPresenter {
     private let view: FeedStoryView
+    private let loadingView: FeedStoryLoadingView
     
-    init(view: FeedStoryView) {
+    init(view: FeedStoryView, loadingView: FeedStoryLoadingView) {
         self.view = view
+        self.loadingView = loadingView
     }
     
     func didStartLoadingStory() {
-        view.display(FeedStoryViewModel(
-            author: nil,
-            title: nil,
-            score: nil,
-            url: nil,
-            isLoading: true,
-            shouldRetry: false))
+        loadingView.display(FeedStoryLoadingViewModel(isLoading: true))
+        view.display(.noStory)
     }
 }
 
@@ -48,36 +58,52 @@ final class FeedStoryPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
-    func test_didStartLoadingStory_displaysLoadingStory() {
+    func test_didStartLoadingStory_displaysLoadingAndNoStory() {
         let (sut, view) = makeSUT()
         
         sut.didStartLoadingStory()
         
-        let message = view.messages.first
-        
-        XCTAssertNil(message?.author)
-        XCTAssertNil(message?.title)
-        XCTAssertNil(message?.score)
-        XCTAssertNil(message?.url)
-        XCTAssertEqual(message?.isLoading, true)
-        XCTAssertEqual(message?.shouldRetry, false)
+        XCTAssertEqual(view.messages, [
+            .display(isLoading: true),
+            .display(viewModel: .noStory)
+        ])
     }
-    
+
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedStoryPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedStoryPresenter(view: view)
+        let sut = FeedStoryPresenter(view: view, loadingView: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
     
-    private class ViewSpy: FeedStoryView {
-        private(set) var messages = [FeedStoryViewModel]()
+    private func makeStory(id: Int = 0,
+                           title: String? = nil,
+                           author: String? = nil,
+                           score: Int? = nil,
+                           createdAt: Date = Date(),
+                           totalComments: Int? = nil,
+                           comments: [Int] = [],
+                           type: String? = nil,
+                           url: URL = URL(string: "https://any-url.com")!) -> Story {
+        Story(id: id, title: title, text: title, author: author, score: score, createdAt: createdAt, totalComments: totalComments, comments: comments, type: type, url: url)
+    }
+    
+    private class ViewSpy: FeedStoryView, FeedStoryLoadingView {
+        enum Message: Hashable {
+            case display(viewModel: FeedStoryViewModel)
+            case display(isLoading: Bool)
+        }
+        private(set) var messages = Set<Message>()
         
         func display(_ viewModel: FeedStoryViewModel) {
-            messages.append(viewModel)
+            messages.insert(.display(viewModel: viewModel))
+        }
+        
+        func display(_ viewModel: FeedStoryLoadingViewModel) {
+            messages.insert(.display(isLoading: viewModel.isLoading))
         }
     }
 }
