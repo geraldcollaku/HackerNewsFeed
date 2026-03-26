2474070b8b4e62f8b9d101ef678cbe287cdcc945
Gerald Collaku <collaku.gerald@gmail.com>
2026-03-26 09:52:49 +0100

    Move `RemoteStoryDataLoader`and `StoryItemMapper` to production

---
+++ HackerNewsFeed/HackerNewsFeed/Feed API/Story/RemoteStoryDataLoader.swift
@@ -0,0 +1,40 @@
+//
+//  RemoteStoryDataLoader.swift
+//  HackerNewsFeed
+//
+//  Created by Gerald Collaku on 26.03.26.
+//
+
+import Foundation
+
+public class RemoteStoryDataLoader {
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    public func loadStory(from url: URL, completion: @escaping (StoryLoader.Result) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+            case let .success((data, response)):
+                completion(Self.map(data, from: response))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> StoryLoader.Result {
+        do {
+            let item = try StoryItemMapper.map(data, from: response)
+            return .success(item)
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+}
---
+++ HackerNewsFeed/HackerNewsFeed/Feed API/Story/StoryItemMapper.swift
@@ -0,0 +1,54 @@
+//
+//  StoryItemMapper.swift
+//  HackerNewsFeed
+//
+//  Created by Gerald Collaku on 26.03.26.
+//
+
+import Foundation
+
+public enum StoryItemMapper {
+    private static var OK_200: Int { 200 }
+
+    private struct Item: Decodable {
+        private let id: Int
+        private let title: String?
+        private let text: String?
+        private let by: String
+        private let score: Int?
+        private let time: Date
+        private let descendants: Int?
+        private let kids: [Int]?
+        private let type: String
+        private let url: URL?
+
+        var model: Story {
+            Story(
+                id: id,
+                title: title,
+                text: text,
+                author: by,
+                score: score,
+                createdAt: time,
+                totalComments: descendants,
+                comments: kids,
+                type: type,
+                url: url
+            )
+        }
+    }
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Story {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        
+        guard response.statusCode == OK_200, let item = try? decoder.decode(Item.self, from: data) else {
+            throw Error.invalidData
+        }
+        return item.model
+    }
+}
