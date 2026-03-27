37c873bf1b63123d33fa754f617c360d5fa8fd18
Gerald Collaku <collaku.gerald@gmail.com>
2026-03-27 08:24:47 +0100

    Remove `default` URLSessionHTTPClient initializer parameter as clients should be mindful which URLSession instance to use. For example clients might decide to use `.shared` instance but when testing we can inject a `URLSession` with an ephermal configuration registered with `URLProtocolStub`. This way we don't need to start/stop requests globally.

--- HackerNewsFeed/HackerNewsFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ HackerNewsFeed/HackerNewsFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -13,11 +13,11 @@ class URLSessionHTTPClientTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        URLProtocolStub.startInterceptingRequests()
+        URLProtocolStub.removeStub()
     }
     
     override func tearDown() {
-        URLProtocolStub.stopInterceptingRequests()
+        URLProtocolStub.removeStub()
         
         super.tearDown()
     }
@@ -88,7 +88,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
-        let sut = URLSessionHTTPClient()
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        let sut = URLSessionHTTPClient(session: session)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
@@ -175,8 +178,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             URLProtocol.registerClass(URLProtocolStub.self)
         }
         
-        static func stopInterceptingRequests() {
-            URLProtocol.unregisterClass(URLProtocolStub.self)
+        static func removeStub() {
             stub = nil
         }
         
