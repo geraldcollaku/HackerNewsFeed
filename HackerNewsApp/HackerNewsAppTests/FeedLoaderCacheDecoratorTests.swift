9d8e60350f30343e3eaad5fefa82339014e6b711
Gerald Collaku <collaku.gerald@gmail.com>
2026-04-03 09:44:40 +0200

    Extract FeedLoader test helpers into a protocol extension to remove duplication

--- HackerNewsApp/HackerNewsAppTests/FeedLoaderCacheDecoratorTests.swift
+++ HackerNewsApp/HackerNewsAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -20,7 +20,7 @@ final class FeedLoaderCacheDecorator: FeedLoader {
     }
 }
 
-class FeedCacheDecoratorTests: XCTestCase {
+class FeedCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     
     func test_load_deliversFeedOnLoaderSuccess() {
         let feed = uniqueIdFeed()
@@ -36,23 +36,5 @@ class FeedCacheDecoratorTests: XCTestCase {
         
         expect(sut, toCompleteWith: .failure(anyNSError()))
     }
-    
-    // MARK: - Helpers
-    
-    private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedFeed), .success(expectedFeed)):
-                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-            case (.failure, .failure):
-                break
-            default:
-                XCTFail("Expected \(expectedResult), got \(receivedResult) result instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1)
-    }
+
 }
