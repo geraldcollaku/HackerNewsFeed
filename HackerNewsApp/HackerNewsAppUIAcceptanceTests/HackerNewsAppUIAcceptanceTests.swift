bfdbbb3c365b5d1ac0774ffa03a4d5c632dae44f
Gerald Collaku <collaku.gerald@gmail.com>
2026-04-04 10:23:35 +0200

    Add UI Test target to test high-level Acceptance Tests

---
+++ HackerNewsApp/HackerNewsAppUIAcceptanceTests/HackerNewsAppUIAcceptanceTests.swift
@@ -0,0 +1,12 @@
+//
+//  HackerNewsAppUIAcceptanceTests.swift
+//  HackerNewsAppUIAcceptanceTests
+//
+//  Created by Gerald Collaku on 04.04.26.
+//
+
+import XCTest
+
+final class HackerNewsAppUIAcceptanceTests: XCTestCase {
+    
+}
