85413a0412ba44e242ce04e1e5b1963cad1998ed
Gerald Collaku <collaku.gerald@gmail.com>
2026-04-06 18:41:01 +0200

    Assert snapshots for light and dark mode using an iPhone 17 configuration

--- HackerNewsFeed/HackerNewsFeediOSTests/FeedSnapshotTests.swift
+++ HackerNewsFeed/HackerNewsFeediOSTests/FeedSnapshotTests.swift
@@ -16,7 +16,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(emptyFeed())
         
-        assert(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+        assert(snapshot: sut.snapshot(for: .iPhone17(style: .light)), named: "EMPTY_FEED_light")
+        assert(snapshot: sut.snapshot(for: .iPhone17(style: .dark)), named: "EMPTY_FEED_dark")
     }
     
     func test_feedWithContent() {
@@ -24,7 +25,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(feedWithContent())
         
-        assert(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
+        assert(snapshot: sut.snapshot(for: .iPhone17(style: .light)), named: "FEED_WITH_CONTENT_light")
+        assert(snapshot: sut.snapshot(for: .iPhone17(style: .dark)), named: "FEED_WITH_CONTENT_dark")
     }
     
     func test_feedWithErrorMessage() {
@@ -32,7 +34,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(.error(message: "This is a \nmulti-line\nerror message"))
         
-        assert(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
+        assert(snapshot: sut.snapshot(for: .iPhone17(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone17(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
     }
     
     func test_feedWithFailedImageLoading() {
@@ -40,7 +43,8 @@ class FeedSnapshotTests: XCTestCase {
         
         sut.display(feedWithFailedStoryLoading())
         
-        assert(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_STORY_LOADING")
+        assert(snapshot: sut.snapshot(for: .iPhone17(style: .light)), named: "FEED_WITH_FAILED_STORY_LOADING_light")
+        assert(snapshot: sut.snapshot(for: .iPhone17(style: .dark)), named: "FEED_WITH_FAILED_STORY_LOADING_dark")
     }
     
     private func makeSUT() -> FeedViewController {
@@ -142,11 +146,63 @@ class FeedSnapshotTests: XCTestCase {
 }
 
 extension UIViewController {
+    func snapshot(for configuration: SnapshotConfiguration) -> UIImage {
+        SnapshotWindow(configuration: configuration, root: self).snapshot()
+    }
+}
+
+private final class SnapshotWindow: UIWindow {
+    private var configuration: SnapshotConfiguration = .iPhone17(style: .light)
+    
+    convenience init(configuration: SnapshotConfiguration, root: UIViewController) {
+        let dummyScene = (UIWindowScene.self as NSObject.Type).init() as! UIWindowScene
+        self.init(windowScene: dummyScene)
+        self.frame = CGRect(origin: .zero, size: configuration.size)
+        self.configuration = configuration
+        self.layoutMargins = configuration.layoutMargings
+        self.rootViewController = root
+        self.isHidden = false
+        root.view.layoutMargins = configuration.layoutMargings
+    }
+    
+    override var safeAreaInsets: UIEdgeInsets {
+        configuration.safeAreaInsets
+    }
+    
+    override var traitCollection: UITraitCollection {
+        return configuration.traitCollection
+    }
+    
     func snapshot() -> UIImage {
-        let render = UIGraphicsImageRenderer(bounds: view.bounds)
+        let render = UIGraphicsImageRenderer(bounds: bounds, format: .init(for: configuration.traitCollection))
         return render.image { action in
-            view.layer.render(in: action.cgContext)
+            layer.render(in: action.cgContext)
         }
+    }
+}
+
+struct SnapshotConfiguration {
+    let size: CGSize
+    let safeAreaInsets: UIEdgeInsets
+    let layoutMargings: UIEdgeInsets
+    let traitCollection: UITraitCollection
+    
+    static func iPhone17(style: UIUserInterfaceStyle, contentSize: UIContentSizeCategory = .medium) -> SnapshotConfiguration {
+        SnapshotConfiguration(
+            size: CGSize(width: 393, height: 852),
+            safeAreaInsets: UIEdgeInsets(top: 62, left: 0, bottom: 34, right: 0),
+            layoutMargings: UIEdgeInsets(top: 70, left: 8, bottom: 42, right: 8),
+            traitCollection: UITraitCollection(mutations: { traits in
+                traits.forceTouchCapability = .unavailable
+                traits.layoutDirection = .leftToRight
+                traits.preferredContentSizeCategory = contentSize
+                traits.userInterfaceIdiom = .phone
+                traits.verticalSizeClass = .regular
+                traits.displayScale = 3
+                traits.displayGamut = .P3
+                traits.userInterfaceStyle = style
+            })
+        )
     }
 }
 
