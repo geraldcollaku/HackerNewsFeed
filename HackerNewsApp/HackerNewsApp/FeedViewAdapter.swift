662eff545a4526cd1383d85ef7f5ba7cae9d0bc5
Gerald Collaku <collaku.gerald@gmail.com>
2026-06-29 17:01:38 +0200

    Reuse existing cell controllers to avoid unnecessary reallocation of resources

--- HackerNewsApp/HackerNewsApp/FeedViewAdapter.swift
+++ HackerNewsApp/HackerNewsApp/FeedViewAdapter.swift
@@ -13,19 +13,27 @@ class FeedViewAdapter: ResourceView {
     private weak var controller: ListViewController?
     private let loader: (Int) -> StoryLoader.Publisher
     private let selection: (FeedId) -> Void
+    private let currentFeed: [FeedId: CellController]
 
     private typealias FeedStoryPresentationAdapter = LoadResourcePresentationAdapter<Story, WeakRefVirtualProxy<FeedStoryCellController>>
 
     private typealias LoadMorePresentationAdapter = LoadResourcePresentationAdapter<Paginated<FeedId>, FeedViewAdapter>
     
-    init(controller: ListViewController, loader: @escaping (Int) -> StoryLoader.Publisher, selection: @escaping (FeedId) -> Void) {
+    init(currentFeed: [FeedId: CellController] = [:], controller: ListViewController, loader: @escaping (Int) -> StoryLoader.Publisher, selection: @escaping (FeedId) -> Void) {
+        self.currentFeed = currentFeed
         self.controller = controller
         self.loader = loader
         self.selection = selection
     }
 
     func display(_ viewModel: Paginated<FeedId>) {
+        guard let controller = controller else { return }
+        var currentFeed = self.currentFeed
         let feed: [CellController] = viewModel.items.map { model in
+            if let controller = currentFeed[model] {
+                return controller
+            }
+            
             let adapter = FeedStoryPresentationAdapter(loader: { [loader] in
                 loader(model.id)
             })
@@ -33,6 +41,7 @@ class FeedViewAdapter: ResourceView {
             let view = FeedStoryCellController(delegate: adapter, selection: { [selection] in
                 selection(model)
             })
+            
             view.onNeedsReconfigure = { [weak controller = self.controller, id = AnyHashable(model)] in
                 controller?.update(id: id)
             }
@@ -43,11 +52,13 @@ class FeedViewAdapter: ResourceView {
                 errorView: WeakRefVirtualProxy(view),
                 mapper: FeedStoryPresenter.map
             )
-            return CellController(id: model, view)
+            let controller = CellController(id: model, view)
+            currentFeed[model] = controller
+            return controller
         }
         
         guard let loadMorePublisher = viewModel.loadMorePublisher else {
-            controller?.display(feed)
+            controller.display(feed)
             return
         }
         
@@ -56,13 +67,16 @@ class FeedViewAdapter: ResourceView {
         let loadMore = LoadMoreCellController(callback: loadMoreAdapter.loadResource)
         
         loadMoreAdapter.presenter = LoadResourcePresenter(
-            resourceView: self,
+            resourceView: FeedViewAdapter(currentFeed: currentFeed,
+                                          controller: controller,
+                                          loader: loader,
+                                          selection: selection),
             loadingView: WeakRefVirtualProxy(loadMore),
             errorView: WeakRefVirtualProxy(loadMore),
             mapper: { $0 })
         
         let loadMoreSection = [CellController(id: UUID(), loadMore)]
         
-        controller?.display(feed, loadMoreSection)
+        controller.display(feed, loadMoreSection)
     }
 }
