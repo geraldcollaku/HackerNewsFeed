969d89a6b6bf61502828fcda6aab96f63e763371
Gerald Collaku <collaku.gerald@gmail.com>
2026-09-07 23:39:10 +0200

    Remove unused async methods

--- HackerNewsFeed/HackerNewsFeed/Feed Cache/FeedStore.swift
+++ HackerNewsFeed/HackerNewsFeed/Feed Cache/FeedStore.swift
@@ -10,68 +10,8 @@ import Foundation
 public typealias CachedFeed = (feed: [LocalFeedId], timestamp: Date)
 
 public protocol FeedStore {
-    typealias DeletionResult = Result<Void, Error>
-    typealias DeletionCompletion = (DeletionResult) -> Void
-    
-    typealias InsertionResult = Result<Void, Error>
-    typealias InsertionCompletion = (InsertionResult) -> Void
-    
-    typealias RetrievalResult = Result<CachedFeed?, Error>
-    typealias RetrievalCompletion = (RetrievalResult) -> Void
-
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to dispatch to appropriate threads, if needed.
-    @available(*, deprecated)
-    func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to dispatch to appropriate threads, if needed.
-    @available(*, deprecated)
-    func insert(_ feed: [LocalFeedId], timestamp: Date, completion: @escaping InsertionCompletion)
-    
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to dispatch to appropriate threads, if needed.
-    @available(*, deprecated)
-    func retrieve(completion: @escaping RetrievalCompletion)
-    
     func deleteCachedFeed() throws
     func insert(_ feed: [LocalFeedId], timestamp: Date) throws
     func retrieve() throws -> CachedFeed?
 }
 
-extension FeedStore {
-    public func deleteCachedFeed() throws {
-        let group = DispatchGroup()
-        group.enter()
-        deleteCachedFeed { _ in
-            group.leave()
-        }
-        group.wait()
-    }
-    
-    public func insert(_ feed: [LocalFeedId], timestamp: Date) throws {
-        let group = DispatchGroup()
-        group.enter()
-        insert(feed, timestamp: timestamp) { _ in
-            group.leave()
-        }
-        group.wait()
-    }
-    
-    public func retrieve() throws -> CachedFeed? {
-        let group = DispatchGroup()
-        group.enter()
-        var result: Result<CachedFeed?, Error>!
-        retrieve {
-            result = $0
-            group.leave()
-        }
-        group.wait()
-        return try result.get()
-    }
-    
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {}
-    public func insert(_ feed: [LocalFeedId], timestamp: Date, completion: @escaping InsertionCompletion) {}
-    public func retrieve(completion: @escaping RetrievalCompletion) {}
-    
-}
--- HackerNewsFeed/HackerNewsFeed/Feed Cache/Infrastructure/CodableFeedStore.swift
+++ HackerNewsFeed/HackerNewsFeed/Feed Cache/Infrastructure/CodableFeedStore.swift
@@ -1,85 +1,85 @@
+////
+////  CodableFeedStore.swift
+////  HackerNewsFeed
+////
+////  Created by Gerald Collaku on 26.01.26.
+////
 //
-//  CodableFeedStore.swift
-//  HackerNewsFeed
+//import Foundation
 //
-//  Created by Gerald Collaku on 26.01.26.
-//
-
-import Foundation
-
-public class CodableFeedStore: FeedStore {
-    private struct Cache: Codable {
-        let feed: [CodableFeedId]
-        let timestamp: Date
-        
-        var localFeed: [LocalFeedId] {
-            return feed.map { $0.local }
-        }
-    }
-    
-    private struct CodableFeedId: Codable {
-        private let id: Int
-        
-        init(_ id: LocalFeedId) {
-            self.id = id.id
-        }
-        
-        var local: LocalFeedId {
-            LocalFeedId(id: id)
-        }
-    }
-    
-    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
-    private let storeURL: URL
-    
-    public init(storeURL: URL) {
-        self.storeURL = storeURL
-    }
-    
-    public func retrieve(completion: @escaping RetrievalCompletion) {
-        let storeURL = self.storeURL
-        queue.async {
-            guard let data = try? Data.init(contentsOf: storeURL) else {
-                return completion(.success(.none))
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let cache = try decoder.decode(Cache.self, from: data)
-                completion(.success(CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp)))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    public func insert(_ feed: [LocalFeedId], timestamp: Date, completion: @escaping InsertionCompletion) {
-        let storeURL = self.storeURL
-        queue.async(flags: .barrier) {
-            do {
-                let encoder = JSONEncoder()
-                let cache = Cache(feed: feed.map(CodableFeedId.init), timestamp: timestamp)
-                let encoded = try encoder.encode(cache)
-                try encoded.write(to: storeURL)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        let storeURL = self.storeURL
-        queue.async(flags: .barrier) {
-            guard FileManager.default.fileExists(atPath: storeURL.path) else {
-                return completion(.success(()))
-            }
-            do {
-                try FileManager.default.removeItem(at: storeURL)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-    }
-}
+//public class CodableFeedStore: FeedStore {
+//    private struct Cache: Codable {
+//        let feed: [CodableFeedId]
+//        let timestamp: Date
+//        
+//        var localFeed: [LocalFeedId] {
+//            return feed.map { $0.local }
+//        }
+//    }
+//    
+//    private struct CodableFeedId: Codable {
+//        private let id: Int
+//        
+//        init(_ id: LocalFeedId) {
+//            self.id = id.id
+//        }
+//        
+//        var local: LocalFeedId {
+//            LocalFeedId(id: id)
+//        }
+//    }
+//    
+//    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
+//    private let storeURL: URL
+//    
+//    public init(storeURL: URL) {
+//        self.storeURL = storeURL
+//    }
+//    
+//    public func retrieve(completion: @escaping RetrievalCompletion) {
+//        let storeURL = self.storeURL
+//        queue.async {
+//            guard let data = try? Data.init(contentsOf: storeURL) else {
+//                return completion(.success(.none))
+//            }
+//            
+//            do {
+//                let decoder = JSONDecoder()
+//                let cache = try decoder.decode(Cache.self, from: data)
+//                completion(.success(CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp)))
+//            } catch {
+//                completion(.failure(error))
+//            }
+//        }
+//    }
+//    
+//    public func insert(_ feed: [LocalFeedId], timestamp: Date, completion: @escaping InsertionCompletion) {
+//        let storeURL = self.storeURL
+//        queue.async(flags: .barrier) {
+//            do {
+//                let encoder = JSONEncoder()
+//                let cache = Cache(feed: feed.map(CodableFeedId.init), timestamp: timestamp)
+//                let encoded = try encoder.encode(cache)
+//                try encoded.write(to: storeURL)
+//                completion(.success(()))
+//            } catch {
+//                completion(.failure(error))
+//            }
+//        }
+//    }
+//    
+//    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+//        let storeURL = self.storeURL
+//        queue.async(flags: .barrier) {
+//            guard FileManager.default.fileExists(atPath: storeURL.path) else {
+//                return completion(.success(()))
+//            }
+//            do {
+//                try FileManager.default.removeItem(at: storeURL)
+//                completion(.success(()))
+//            } catch {
+//                completion(.failure(error))
+//            }
+//        }
+//    }
+//}
