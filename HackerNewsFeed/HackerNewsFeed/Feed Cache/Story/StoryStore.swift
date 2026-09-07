73acbb279e6885b470ac4f35f028a3c8830f7204
Gerald Collaku <collaku.gerald@gmail.com>
2026-09-07 23:40:45 +0200

    Remove unusde async methods

--- HackerNewsFeed/HackerNewsFeed/Feed Cache/Story/StoryStore.swift
+++ HackerNewsFeed/HackerNewsFeed/Feed Cache/Story/StoryStore.swift
@@ -8,43 +8,6 @@
 import Foundation
 
 public protocol StoryStore {
-    typealias RetrievalResult = Swift.Result<LocalStory?, Error>
-    typealias InsertionResult = Swift.Result<Void, Error>
-    
     func insert(story: LocalStory) throws
     func retrieve(for id: Int) throws -> LocalStory?
-    
-    @available(*, deprecated)
-    func retrieve(for id: Int, completion: @escaping (RetrievalResult) -> Void)
-    @available(*, deprecated)
-    func insert(_ story: LocalStory, completion: @escaping (InsertionResult) -> Void)
-}
-
-public extension StoryStore {
-    func insert(story: LocalStory) throws {
-        let group = DispatchGroup()
-        group.enter()
-        var result: InsertionResult!
-        insert(story){
-            result = $0
-            group.leave()
-        }
-        group.wait()
-        return try result.get()
-    }
-    
-    func retrieve(for id: Int) throws -> LocalStory? {
-        let group = DispatchGroup()
-        group.enter()
-        var result: RetrievalResult!
-        retrieve(for: id) {
-            result = $0
-            group.leave()
-        }
-        group.wait()
-        return try result.get()
-    }
-    
-    func retrieve(for id: Int, completion: @escaping (RetrievalResult) -> Void) {}
-    func insert(_ story: LocalStory, completion: @escaping (InsertionResult) -> Void) {}
 }
